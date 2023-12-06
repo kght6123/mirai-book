30e375176676c775d5e74debeed88dd3b6ac6ed7
kght6123 <i.feeling.song+github@gmail.com>
2023-12-07 00:51:14 +0900

    Printed CSSの一部をTailwind CSSのプラグイン化した

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,59 @@
+import plugin from 'tailwindcss/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	corePlugins: {
+  corePlugins: {
     preflight: false,
   },
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  separator: '_',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    counterIncrement: {
+      none: 'none',
+      page: 'page',
+    },
+    breakBefore: {
+      auto: 'auto',
+      avoid: 'avoid',
+      always: 'always',
+      all: 'all',
+      avoidPage: 'avoid-page',
+      page: 'page',
+      left: 'left',
+      right: 'right',
+      recto: 'recto',
+      verso: 'verso',
+      avoidColumn: 'avoid-column',
+      column: 'column',
+      avoidRegion: 'avoid-region',
+      region: 'region',
+    },
+    extend: {},
+  },
+  plugins: [
+    plugin(function({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          ci: (value) => ({
+            counterIncrement: value
+          }),
+          "target-counter": (value) => ({
+            content: `leader(".") target-counter(attr(href),${value})`
+          }),
+          "counter": (value) => ({
+            content: `counter(${value})`
+          }),
+        },
+        { values: theme('counterIncrement') }
+      )
+      matchUtilities(
+        {
+          "break-bf": (value) => ({
+            breakBefore: value
+          }),
+        },
+        { values: theme('breakBefore') }
+      )
+    }),
+  ],
 }
