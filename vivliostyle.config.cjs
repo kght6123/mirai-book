9b4c9d3f88c79797e36e10460d17c30e349d1c81
kght6123 <i.feeling.song+github@gmail.com>
2023-12-08 23:57:22 +0900

    エントリーのタイトル指定が不要なので消した

--- vivliostyle.config.cjs
+++ vivliostyle.config.cjs
@@ -4,10 +4,7 @@ module.exports = {
   language: "ja",
   size: "JIS-B5",
   entry: [
-    {
-      path: "./dist/index.html",
-      title: "index",
-    },
+    "./dist/index.html",
   ], // `entry` can be `string` or `object` if there's only single markdown file.
   workspaceDir: ".vivliostyle", // directory which is saved intermediate files.
   toc: false, // whether generate and include ToC HTML or not, default to 'false'.
